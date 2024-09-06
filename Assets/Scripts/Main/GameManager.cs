7a4f298f85a5da8acede5339a498cdc6149851b3
binmuxiz <leb001122@icloud.com>
2024-09-06 17:56:33 +0900

    StartGame 진행중

--- Assets/Scripts/Main/GameManager.cs
+++ Assets/Scripts/Main/GameManager.cs
@@ -3,9 +3,31 @@ using UnityEditor;
 #endif
 
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    // UnityEvent를 사용하여 LoadingCanvas 활성화 제어
+    public UnityEvent onGameStart;
+    
+    public void StartGame()
+    {
+        Debug.Log("StartGame()");
+        // Loading Cavas 켜기
+        // onGameStart.Invoke();
+        
+        // 일정 시간 대기
+        
+        
+        
+        // Game Scene Temp 로드
+    }
+    
+    // private IEnumerator LoadGameScene()
+    // {
+    //     
+    // }
+    
     public void QuitGame()
     {
         Application.Quit();
