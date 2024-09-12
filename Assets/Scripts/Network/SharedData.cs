e51082d65f0c3855e4395dc02ddd0ee50bcb9318
willium02 <121286794+willium02@users.noreply.github.com>
2024-09-12 13:57:27 +0900

    김민준 개바보

---
+++ Assets/Scripts/Network/SharedData.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Threading;
+using Fusion;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SharedData : NetworkBehaviour
+{
+    
+    public static SharedData Instance;
+    
+    
+    static int CountReady { get; set; }
+
+    private int MaxCount { get; set; } =2;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+    }
+
+    [Rpc(RpcSources.All, RpcTargets.All)]
+    public void CheckReadyRpc()
+    {
+        CountReady++;
+        Debug.Log(CountReady);
+
+        if (CountReady >= 2)
+        {
+            RunnerController.Runner.LoadScene(SceneRef.FromIndex(2));
+        }
+        
+    }
+
+    
+    
+   
+    
+    
+    
+    
+    
+    
+}
